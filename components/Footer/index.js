e3914be6b85ab8e1a1a6f23a15357ea7a430e208
Abhi <118065119+bimazed@users.noreply.github.com>
2024-06-27 17:56:29 +0530

    Update index.js new resume link

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,7 +16,7 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-             <Link href="https://drive.google.com/file/d/1SKpu_1vjFlZIasFiFVBSWRC9H3NbIo-z/view?usp=drivesdk">
+             <Link href="https://drive.google.com/file/d/1YP9dqX1yOWny2wn-BGWFGacgD1JWwno1/view?usp=drivesdk">
             <Button>See My Resume</Button>
             </Link>
             <div className="mt-10">
